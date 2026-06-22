823ff28ea1045977c0a571ac757ca1f32d03845d
mateuszs0121-bit <mateuszs0121@outlook.com>
2026-06-22 22:48:17 +0200

    zadanie ze zmiennych

---
+++ Zadania/main.js
@@ -0,0 +1,17 @@
+let myVariable = 10;
+console.log(myVariable);
+let myName = "Benedykt";
+console.log(myName);
+
+console.log(typeof myVariable);
+console.log(typeof myName);
+
+myVariable = 11;
+console.log(myVariable);
+myName = "Mieszko";
+console.log(myName);
+
+const sum = myVariable + 5;
+console.log(sum);
+const greeting = myName + ", witaj w świecie JavaScript!";
+console.log(greeting);
