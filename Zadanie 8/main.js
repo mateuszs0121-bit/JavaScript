b06451220518d4f84af965bd53e85261c4cc2b56
mateuszs0121-bit <mateuszs0121@outlook.com>
2026-08-25 13:50:42 +0200

    poprzednie zadanie zrobione na inny sposob

--- Zadanie 8/main.js
+++ Zadanie 8/main.js
@@ -1,8 +1,22 @@
 document.addEventListener("click", function (e) {
   console.log(e.clientX, e.clientY);
-  if (e.clientX % 2 == 0 && e.clientY % 2 == 0)
-    document.body.style.backgroundColor = "red";
-  else if (e.clientX % 2 != 0 && e.clientY % 2 != 0)
-    document.body.style.backgroundColor = "blue";
-  else document.body.style.backgroundColor = "green";
+  // if (e.clientX % 2 == 0 && e.clientY % 2 == 0)
+  //   document.body.style.backgroundColor = "red";
+  // else if (e.clientX % 2 != 0 && e.clientY % 2 != 0)
+  //   document.body.style.backgroundColor = "blue";
+  // else document.body.style.backgroundColor = "green";
+  const color = getColor(e);
+  document.body.style.backgroundColor = color;
 });
+
+function getColor(e) {
+  if (e.clientX % 2 === 0) {
+    if (e.clientY % 2 === 0) {
+      return "red";
+    } else return "green";
+  } else {
+    if (e.clientY % 2 === 0) {
+      return "green";
+    } else return "blue";
+  }
+}
