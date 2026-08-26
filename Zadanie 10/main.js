4f60e8210f5058d9e676d8f026f9fa746c1be679
mateuszs0121-bit <mateuszs0121@outlook.com>
2026-08-26 18:11:13 +0200

    sterowanie kolorem za pomocą strzałek

---
+++ Zadanie 10/main.js
@@ -0,0 +1,34 @@
+let red = 100;
+let green = 100;
+let blue = 100;
+
+document.body.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`;
+
+const changeColor = (e) => {
+  //   console.log(e.keyCode, e.which);
+  //   if (e.keyCode == 38 && red < 255) {
+  //     //   console.log("strzałka w górę");
+  //     red++;
+  //     green++;
+  //     blue++;
+  //     document.body.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`;
+  //   } else if (e.keyCode == 40 && red >= 0) {
+  //     red--;
+  //     green--;
+  //     blue--;
+  //     document.body.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`;
+  //     // console.log("strzałka w dół");
+  //   }
+  //   console.log(red);
+
+  switch (e.keyCode) {
+    case 38:
+      document.body.style.backgroundColor = `rgb(${red <= 255 ? red++ : red}, ${green <= 255 ? green++ : green}, ${blue <= 255 ? blue++ : blue})`;
+      break;
+    case 40:
+      document.body.style.backgroundColor = `rgb(${red >= 0 ? red-- : red}, ${green >= 0 ? green-- : green}, ${blue >= 0 ? blue-- : blue})`;
+      break;
+  }
+};
+
+window.addEventListener("keydown", changeColor);
