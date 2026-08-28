14e6edfecdf09c831390a291c31f1a1911523493
mateuszs0121-bit <mateuszs0121@outlook.com>
2026-08-28 18:23:29 +0200

    używanie wielu haseł

--- Zadanie 11/main.js
+++ Zadanie 11/main.js
@@ -1,15 +1,24 @@
 const input = document.getElementById("pass");
 const div = document.querySelector(".message");
-const password = "user";
-const message = "Hasło poprawne";
-input.addEventListener("input", (e) => {
-  console.log(e.target.value);
-  if (password === e.target.value) {
-    div.textContent = message;
-    e.target.value = "";
-  } else {
-    div.textContent = "";
-  }
+const passwords = ["user", "wiosna", "Ania"];
+const messages = ["Hasło poprawne", "Piękna pora roku", "tak to ona"];
+input.addEventListener("input", function (e) {
+  div.textContent = "";
+  const text = e.target.value;
+  passwords.forEach((password, index) => {
+    if (password === text) {
+      div.textContent = messages[index];
+      e.target.value = "";
+    }
+  });
+  // console.log(this.value);
+  // console.log(e.target.value);
+  // if (passwords === e.target.value) {
+  //   div.textContent = messages;
+  //   e.target.value = "";
+  // } else {
+  //   div.textContent = "";
+  // }
 });
 input.addEventListener("focus", (e) => {
   e.target.classList.add("active");
