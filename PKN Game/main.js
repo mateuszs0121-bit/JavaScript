10e8eb8f5f64752503b31b75125a228b143c3a8a
mateuszs0121-bit <mateuszs0121@outlook.com>
2026-09-04 17:31:10 +0200

    dodanie liczników i wyświetlanie wyników na głównej stronie

--- PKN Game/main.js
+++ PKN Game/main.js
@@ -12,10 +12,12 @@ const game = {
 
 const hands = [...document.querySelectorAll(".select img")];
 
+// const gameResult = checkResult();
+
 function handSelection() {
   console.log(this);
   game.playerHand = this.dataset.option;
-  console.log(game.playerHand);
+  console.log(`Player: ${game.playerHand}`);
   hands.forEach((hand) => (hand.style.boxShadow = ""));
   this.style.boxShadow = "0 0 0 4px green";
 }
@@ -24,10 +26,51 @@ function aiChoice() {
   return hands[Math.floor(Math.random() * 3)].dataset.option;
 }
 
+function checkResult(player, ai) {
+  if (player === ai) {
+    console.log("Remis");
+    return "draw";
+  } else if (
+    (player === "papier" && ai === "kamień") ||
+    (player === "kamień" && ai === "nożyce") ||
+    (player === "nożyce" && ai === "papier")
+  ) {
+    console.log("Punkt dla użytkownika");
+    return "win";
+  } else console.log("Punkt dla AI");
+  return "loss";
+}
+
+function publishResult(player, ai, result) {
+  document.querySelector("[data-summary='your-choice']").textContent = player;
+  document.querySelector("[data-summary='ai-choice']").textContent = ai;
+  document.querySelector("p.numbers span").textContent = ++gameSummary.numbers;
+
+  if (result === "win") {
+    document.querySelector("p.wins span").textContent = ++gameSummary.wins;
+    document.querySelector("[data-summary='winner']").textContent = "Wygrałeś!";
+    document.querySelector("[data-summary='winner']").style.color = "green";
+  } else if ((result = "loss")) {
+    document.querySelector("p.losses span").textContent = ++gameSummary.losses;
+    document.querySelector("[data-summary='winner']").textContent =
+      "Przegrałeś!";
+    document.querySelector("[data-summary='winner']").style.color = "red";
+  } else {
+    document.querySelector("p.draws span").textContent = ++gameSummary.draws;
+    document.querySelector("[data-summary='winner']").textContent = "Remis!";
+  }
+}
+
 function startGame() {
-  if (!game.playerHand) return alert("Wybierz dłoń, aby rozpocząć grę!");
+  if (!game.playerHand) {
+    return alert("Wybierz dłoń, aby rozpocząć grę!");
+  }
   console.log("Przechodzę dalej");
   game.aiHand = aiChoice();
+  console.log(`AI: ${game.aiHand}`);
+  const gameResult = checkResult(game.playerHand, game.aiHand);
+  console.log(gameResult);
+  publishResult(game.playerHand, game.aiHand, gameResult);
 }
 
 hands.forEach((hand) => hand.addEventListener("click", handSelection));
