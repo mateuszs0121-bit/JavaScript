e060efb303a1763ec5c5dc3d03b38a454487d3ec
mateuszs0121-bit <mateuszs0121@outlook.com>
2026-09-10 18:19:58 +0200

    zastosowanie metody closure

--- Zadanie 20/main.js
+++ Zadanie 20/main.js
@@ -1,8 +1,12 @@
-let number = 0;
-
-const counter = () => {
-  number++;
-  document.body.textContent = `Aktualny stan licznika to ${number}`;
+const add = (firstInt = 0) => {
+  let number = firstInt;
+  return () => {
+    number++;
+    document.body.textContent = `Aktualny stan licznika to ${number}`;
+  };
 };
 
-document.addEventListener("click", counter);
+const counter = add();
+const countFrom5 = add(5);
+
+document.addEventListener("click", countFrom5);
