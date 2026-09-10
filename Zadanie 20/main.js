88b32df0a3047ff2168ce214e060cd295d987e55
mateuszs0121-bit <mateuszs0121@outlook.com>
2026-09-10 18:27:05 +0200

    dalsze ćwiczenie mechanizmu closure

--- Zadanie 20/main.js
+++ Zadanie 20/main.js
@@ -6,6 +6,25 @@ const add = (firstInt = 0) => {
   };
 };
 
+const user = (name = "", age) => {
+  let userName = name;
+  let userAge = age;
+
+  function showName() {
+    console.log(
+      `Cześć ${userName}, ${userAge >= 18 ? "możesz kupić monsterka" : "nie możesz kupić monsterka"}`,
+    );
+  }
+  return showName;
+};
+
+const mateusz = user("Mateusz", 25);
+const dzieciak = user("Brine", 15);
+const noName = user();
+mateusz();
+dzieciak();
+noName();
+
 const counter = add();
 const countFrom5 = add(5);
 
