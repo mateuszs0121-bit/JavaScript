bff5adedb60f272a42a46c35a7d22d766f1db1af
mateuszs0121-bit <mateuszs0121@outlook.com>
2026-09-11 20:38:15 +0200

    prosta stronka odliczająca czas

---
+++ Zadanie 23/main.js
@@ -0,0 +1,28 @@
+const endTime = new Date("2027-01-29 12:00:00").getTime();
+const spanD = document.querySelector("span.d");
+const spanH = document.querySelector("span.h");
+const spanM = document.querySelector("span.m");
+const spanS = document.querySelector("span.s");
+
+setInterval(() => {
+  const nowTime = new Date().getTime();
+  const time = Math.floor((endTime - nowTime) / 1000);
+  const days = Math.floor(
+    endTime / (1000 * 60 * 60 * 24) - nowTime / (1000 * 60 * 60 * 24),
+  );
+  console.log(days);
+  console.log(time);
+
+  let hours =
+    Math.floor(endTime / (1000 * 60 * 60) - nowTime / (1000 * 60 * 60)) % 24;
+  hours = hours < 10 ? `0${hours}` : hours;
+  let minutes = Math.floor(endTime / (1000 * 60) - nowTime / (1000 * 60)) % 60;
+  minutes = minutes < 10 ? `0${minutes}` : minutes;
+  let seconds = Math.floor(endTime / 1000 - nowTime / 1000) % 60;
+  seconds = seconds < 10 ? `0${seconds}` : seconds;
+
+  spanD.textContent = days;
+  spanH.textContent = hours;
+  spanM.textContent = minutes;
+  spanS.textContent = seconds;
+}, 1000);
